6e6e6f616c26a6ef7155733370afac09ed9f6566
Joshua Hill <josh@jlhworkshop.com>
2019-08-07 22:26:45 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+  end
+  
+  def name
+    @name = name
+  end
+  
+  def name=(newName)
+    @name = newName
+  end
+  
+end
